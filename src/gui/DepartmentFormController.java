61f4e0b740c25ef9152e27c81f8220cc95de3758
lwcios <lwciomawro@hotmail.com>
2021-07-26 19:25:09 -0300

    Observer design pattern to update tableview

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -22,6 +25,9 @@ public class DepartmentFormController  implements Initializable{
 	private Department entity;
 	
 	private DepartmentService service;
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+	
 	
 	@FXML
 	private TextField txtId;
@@ -48,6 +54,12 @@ public class DepartmentFormController  implements Initializable{
 	}
 	
 	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+		
+	}
+	
+	
 	@FXML
 	public void onbtSavingAction(ActionEvent event) {
 		if(entity == null) {
@@ -59,6 +71,7 @@ public class DepartmentFormController  implements Initializable{
 		try {
 		entity = getFormData();
 		service.saveOrUpdate(entity);
+		notifyDataChangeListeners();
 		//fechando a janela apos salvar o objeto
 		Utils.currentStage(event).close();
 		
@@ -68,6 +81,14 @@ public class DepartmentFormController  implements Initializable{
 		
 	}
 	
+	private void notifyDataChangeListeners() {
+		
+		for(DataChangeListener listeners : dataChangeListeners) {
+			listeners.onDataChanged();
+		}
+		
+	}
+
 	private Department getFormData() {
 		Department obj = new Department();
 		obj.setId(gui.util.Utils.tryparseToInt(txtId.getText()));
