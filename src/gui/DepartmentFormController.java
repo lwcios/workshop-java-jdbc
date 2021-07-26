419036dfb415d987792eed699a393b2b32f98a39
lwcios <lwciomawro@hotmail.com>
2021-07-26 17:59:30 -0300

    Saving a new department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,9 +3,14 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -44,11 +49,22 @@ public class DepartmentFormController  implements Initializable{
 	
 	
 	@FXML
-	public void onbtSavingAction() {
-		
+	public void onbtSavingAction(ActionEvent event) {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
 		entity = getFormData();
 		service.saveOrUpdate(entity);
-	
+		//fechando a janela apos salvar o objeto
+		Utils.currentStage(event).close();
+		
+		}catch(DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
 		
 	}
 	
@@ -60,8 +76,8 @@ public class DepartmentFormController  implements Initializable{
 	}
 
 	@FXML
-	public void onbtCancelAction() {
-		System.out.println("onbtCancelAction");
+	public void onbtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 	
 	
